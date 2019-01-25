0b8542bdd5e79bd3eac93c091f7f9e947999e65d
fufukha <wafakhalid29@gmail.com>
2019-01-24 19:58:52 -0500

    made timer a dummy component, removed start of func but render func, pass millisec prop

--- src/components/timer/timer.js
+++ src/components/timer/timer.js
@@ -3,64 +3,16 @@ import React, { Component } from 'react';
 export default class Timer extends Component {
   constructor() {
     super();
-
-    this.state = {
-      startTime: null
-    }
-
-    this.timerId = 0;
-    this._startTimer = this._startTimer.bind(this);
-    this._timer = this._timer.bind(this);
-    this._stopTimer = this._stopTimer.bind(this);
-    this._resetTimer = this._resetTimer.bind(this);
-    this._timeToText = this._timeToText.bind(this);
-  }
-
-  componentDidUpdate(prevProps) {
-    if(!prevProps.isActive && this.props.isActive) this._startTimer();
-    if(prevProps.isActive && !this.props.isActive) this._stopTimer();
   }
 
   render() {
-    const timeText = this.state.startTime == null ? '000' : this._timeToText(this._timeLapse());
+    const { milliseconds } = this.props;
+    const timeText = Math.floor(milliseconds / 1000).toString().padStart(3, 0);
 
     return (
       <div className='scoreBoard-numbers'>888
         <div>{timeText}</div>
       </div>
     );
-  }
-
-  _startTimer() {
-    this.setState({ startTime: (new Date()).getTime() });
-
-    if(this.timerId == 0) {
-      this.timerId = setInterval(this._timer, 1000);
-    }
-  }
-
-  _timer() {
-    this.setState({ a: Math.random() }) //trigger render() with a random change
-    if(this._timeLapse() > 999000) this._stopTimer();
-  }
-
-  _stopTimer() {
-    clearInterval(this.timerId);
-  }
-
-  _timeLapse() {
-    return (new Date()).getTime() - this.state.startTime;
-  }
-
-  _resetTimer() {
-    this.setState({
-      timerText: '000',
-      seconds: 0
-    })
-    this.timerId = 0;
-  }
-
-  _timeToText(milliseconds) {
-    return Math.floor(milliseconds / 1000).toString().padStart(3, 0);
   }
 }
