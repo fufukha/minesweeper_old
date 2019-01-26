b40ef0ba8f18c465b878ae93b8340ede2b76d8ea
fufukha <wafakhalid29@gmail.com>
2019-01-26 09:01:52 -0500

    set className to tileState conditionals, added conditionals for return value

--- src/components/game-tile/game-tile.js
+++ src/components/game-tile/game-tile.js
@@ -6,15 +6,19 @@ export default class GameTile extends Component {
 	}
 
   render() {
-    const { value, index, updateFlags,
+    const { value, index, updateFlags, tileState,
 			setToDisplay, setToFlag } = this.props;
 		const [i, j] = [index[0], index[1]]
 
     return (
       <div
-        className='hide'
+        className={tileState == 'flag' ? 'hide' : tileState}
         onContextMenu={(event) => setToFlag(event, i, j)}
         onClick={(event) => setToDisplay(event, i, j)}>
+				{tileState != 'hide' && (
+					tileState == 'flag' ?
+							'📍' : (value === true ? '💣' : value)
+				)}
       </div>
     );
   }
