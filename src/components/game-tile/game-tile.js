c1dd946068000c444b34e17df069a853019a567e
fufukha <wafakhalid29@gmail.com>
2019-01-25 22:26:12 -0500

    made into a dummy componont, pass setToFlag prop

--- src/components/game-tile/game-tile.js
+++ src/components/game-tile/game-tile.js
@@ -3,52 +3,19 @@ import React, { Component } from 'react';
 export default class GameTile extends Component {
 	constructor() {
 		super();
-
-    this.state = {
-      isFlagged: false
-    }
-
-		this._rightClick = this._rightClick.bind(this);
 	}
 
-	// componentDidUpdate(prevState) {
-	// 	const { updateMinesFlagged } = this.props;
-	//
-	// 	if(prevState.isFlagged && !this.state.isFlagged) {
-	// 		updateMinesFlagged(-1);
-	// 	}
-	// 	if(!prevState.isFlagged && this.state.isFlagged) {
-	// 		updateMinesFlagged(1);
-	// 	}
-	// }
-
   render() {
-    const { value, index, updateFlags, setToDisplay } = this.props;
-    const { isFlagged } = this.state;
+    const { value, index, updateFlags,
+			setToDisplay, setToFlag } = this.props;
+		const [i, j] = [index[0], index[1]]
 
     return (
       <div
         className='hide'
-        onContextMenu={this._rightClick}
-        onClick={(event) => setToDisplay(event, index)}>
-				{isFlagged ? '📍' : ''}
+        onContextMenu={(event) => setToFlag(event, i, j)}
+        onClick={(event) => setToDisplay(event, i, j)}>
       </div>
     );
   }
-
-	_rightClick(event) {
-		event.preventDefault();
-		const { updateMinesFlagged } = this.props;
-    const { isFlagged } = this.state;
-
-		this.setState({ isFlagged: !isFlagged });
-
-		if(isFlagged) {
-			updateMinesFlagged(-1);
-			event.target.removeEventListener('click', this._leftClick, false);
-		} else {
-			updateMinesFlagged(1);
-			event.target.addEventListener('click', this._leftClick);
-		}
-	}
 }
